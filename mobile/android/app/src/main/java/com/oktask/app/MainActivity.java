7f9633dd7e0abcee5650e1b627cddede9ee81ef3
jcruz2005 <jcruz23.js@gmail.com>
2026-08-18 15:58:11 -0300

    fix: rebuild Android project properly with Capacitor, add .gitignore for build artifacts

--- mobile/android/app/src/main/java/com/oktask/app/MainActivity.java
+++ mobile/android/app/src/main/java/com/oktask/app/MainActivity.java
@@ -1,15 +1,5 @@
 package com.oktask.app;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-/**
- * OKtask - Gestor de tareas con Pomodoro
- * Activity principal para Android
- */
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-}
+public class MainActivity extends BridgeActivity {}
